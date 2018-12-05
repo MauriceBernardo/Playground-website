00afc032c2c25ab977cc4ed352bdbf55a29cc9a1
MauriceBernardo <Maurice_tikus@hotmail.com>
2018-12-05 17:56:29 +0700

    Testing to add carousels to the index.vue

--- components/Mainmenu.vue
+++ components/Mainmenu.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="header">
-    <b-navbar 
-      toggleable="md" 
-      type="dark" 
-      variant="primary"
-      class="header__navbar">
-      <b-navbar-nav>
-        <nuxt-link 
-          exact 
-          to="./" 
-          class="header__option">Home</nuxt-link>
-        <nuxt-link 
-          to="./Calc" 
-          class="header__option">Calculator</nuxt-link>
-        <nuxt-link 
-          to="./Subscription" 
-          class="header__option">Subscription</nuxt-link>
-      </b-navbar-nav>
-    </b-navbar>
-  </div>
+  <b-navbar 
+    toggleable="md" 
+    type="dark" 
+    variant="primary"
+    class="header__navbar sticky-top bd-navbar">
+    <b-navbar-nav>
+      <nuxt-link 
+        exact 
+        to="./" 
+        class="header__option">Home</nuxt-link>
+      <nuxt-link 
+        to="./Calc" 
+        class="header__option">Calculator</nuxt-link>
+      <nuxt-link 
+        to="./Subscription" 
+        class="header__option">Subscription</nuxt-link>
+    </b-navbar-nav>
+  </b-navbar>
 </template>
 
 <script>
@@ -27,16 +25,11 @@ export default {}
 
 <style lang='scss' scoped>
 .header {
-  &__navbar {
-    position: fixed;
-    width: 100%;
-  }
-
   &__option {
     font-family: cursive;
     margin: 5px 20px;
     text-decoration: none;
-    color: lightgray;
+    color: #000335;
   }
 }
 </style>
